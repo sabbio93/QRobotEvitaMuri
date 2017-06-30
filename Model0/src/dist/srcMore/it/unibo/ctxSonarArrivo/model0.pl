ab5aa23962683b0f92a8c105216380ebfed36bc2
andrea <sabbio.a93@gmail.com>
2017-06-30 17:35:44 +0200

    creata pagina sito del progetto

--- Model0/src/dist/srcMore/it/unibo/ctxSonarArrivo/model0.pl
+++ Model0/src/dist/srcMore/it/unibo/ctxSonarArrivo/model0.pl
@@ -1,8 +1,8 @@
 %====================================================================================
 % Context ctxSonarArrivo  SYSTEM-configuration: file it.unibo.ctxSonarArrivo.model0.pl 
 %====================================================================================
-context(ctxrover, "192.168.251.1",  "TCP", "8070" ).  		 
-context(ctxsonararrivo, "192.168.251.3",  "TCP", "8035" ).  		 
+context(ctxrover, "localhost",  "TCP", "8070" ).  		 
+context(ctxsonararrivo, "localhost",  "TCP", "8035" ).  		 
 %%% -------------------------------------------
 qactor( sonarb , ctxsonararrivo, "it.unibo.sonarb.MsgHandle_Sonarb"   ). %%store msgs 
 qactor( sonarb_ctrl , ctxsonararrivo, "it.unibo.sonarb.Sonarb"   ). %%control-driven 
