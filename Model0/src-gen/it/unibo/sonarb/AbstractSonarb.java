312a523bab84f0a384af5391d3875b863a79b3ea
aziro <ziro.alberto@gmail.com>
2017-07-01 19:44:00 +0200

    Rinominazione dell'evento emesso quando il robot arriva al sonar di arrivoB (Issue #13)

--- Model0/src-gen/it/unibo/sonarb/AbstractSonarb.java
+++ Model0/src-gen/it/unibo/sonarb/AbstractSonarb.java
@@ -72,11 +72,11 @@ public abstract class AbstractSonarb extends QActor {
 	    		}
 	    		//onEvent
 	    		if( currentEvent.getEventId().equals("sonar") ){
-	    		 		String parg="stop";
+	    		 		String parg="roverOnB";
 	    		 		/* RaiseEvent */
 	    		 		parg = updateVars(Term.createTerm("sonar(Nome,Oggetto,Distanza)"),  Term.createTerm("sonar(sonar2,rover,D)"), 
 	    		 			    		  					Term.createTerm(currentEvent.getMsg()), parg);
-	    		 		if( parg != null ) emit( "stop", parg );
+	    		 		if( parg != null ) emit( "roverOnB", parg );
 	    		 }
 	    		if( planUtils.repeatPlan(nPlanIter,0).getGoon() ) continue;
 	    break;
