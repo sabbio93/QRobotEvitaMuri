dcb4a09d6d77363c8a8d6e5c48d76f3cb1c5e323
Alberto Zirondelli <ziro.alberto@gmail.com>
2017-07-05 18:24:14 +0200

    Aggiunta console appropriata per model1

--- Model1/src-gen/it/unibo/ctxRover/MainCtxRover.java
+++ Model1/src-gen/it/unibo/ctxRover/MainCtxRover.java
@@ -25,7 +25,7 @@ private it.unibo.qactor.robot.RobotActor robot;
 	public static void main(String[] args) throws Exception{
 			IOutputEnvView outEnvView = SituatedSysKb.standardOutEnvView;
 			it.unibo.qactors.QActorUtils.setRobotBase("mock" );  
-		    String webDir = null;
+		    String webDir = "./srcMore/it/unibo/ctxRover";
 			QActorContext.initQActorSystem(
 				"ctxrover", "./srcMore/it/unibo/ctxRover/m1.pl", 
 				"./srcMore/it/unibo/ctxRover/sysRules.pl", outEnvView,webDir, false);
