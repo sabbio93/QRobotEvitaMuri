236b42fe53c13f1855f553230f9fc3440c7bd011
Riccardo <riccardo.buscaroli@studio.unibo.it>
2017-07-11 00:01:19 +0200

    Model1 e Model4 adattati per robot fisico

--- Utils/buildAll.sh
+++ Utils/buildAll.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 gradle="../gradlew"
 [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ] && gradle="gradlew.bat"
-for d in ../Model* ; do
-	for f in $d/b*.gradle; do
-        $gradle -b "$f" clean eclipse build
-    done
+
+for f in b*.gradle; do
+    $gradle -b "$f" eclipse build
 done
