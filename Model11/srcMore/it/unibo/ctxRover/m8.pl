a0ca7958cc473c55b0659ed4a6f07709758c3e61
andrea <sabbio.a93@gmail.com>
2017-07-13 10:16:29 +0200

    aggiunto println che giustifica la react action in m0 al posto dell actorop

---
+++ Model11/srcMore/it/unibo/ctxRover/m8.pl
@@ -0,0 +1,15 @@
+%====================================================================================
+% Context ctxRover  SYSTEM-configuration: file it.unibo.ctxRover.m8.pl 
+%====================================================================================
+context(ctxcontroller, "127.0.0.1",  "TCP", "8040" ).  		 
+context(ctxrover, "localhost",  "TCP", "8070" ).  		 
+%%% -------------------------------------------
+qactor( mockstart , ctxcontroller, "it.unibo.mockstart.MsgHandle_Mockstart"   ). %%store msgs 
+qactor( mockstart_ctrl , ctxcontroller, "it.unibo.mockstart.Mockstart"   ). %%control-driven 
+qactor( robotmind , ctxcontroller, "it.unibo.robotmind.MsgHandle_Robotmind"   ). %%store msgs 
+qactor( robotmind_ctrl , ctxcontroller, "it.unibo.robotmind.Robotmind"   ). %%control-driven 
+%%% -------------------------------------------
+%%% -------------------------------------------
+qactor( rover , ctxrover, "it.unibo.rover.MsgHandle_Rover" ). 
+qactor( rover_ctrl , ctxrover, "it.unibo.rover.Rover" ). 
+
