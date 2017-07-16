2e964b7dbca003c80b2c0b71608b28c0214a67a8
aziro <ziro.alberto@gmail.com>
2017-07-16 23:25:45 +0200

    Modificata inizializzazione Unity

--- Model0/srcMore/it/unibo/rover/WorldTheory.pl
+++ Model0/srcMore/it/unibo/rover/WorldTheory.pl
@@ -136,6 +136,7 @@ actorPrintln( X ):- actorobj(A), text_term(XS,X), A  <- println( XS ).
 %-------------------------------------------------
 %  User static rules about rover
 %------------------------------------------------- 
+unity( Host):-getCtxHost( ctxUnity,Host).
 /*
 ------------------------------------------------------------------------
 testex :- actorPrintln( testex ),
